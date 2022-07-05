644b4de3db69361f6b3bd102effad7b74a050c15
kinhhghe153111 <kinhhghe153111@fpt.edu.vn>
2022-07-05 14:44:19 +0700

    feat: no unique bean definition exception

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -8,11 +8,25 @@ import org.springframework.context.annotation.Configuration;
 public class ProjectConfig {
 
     @Bean
-    public Vehicle vehicle(){
+    public Vehicle vehicle1(){
         Vehicle vehicle = new Vehicle();
         vehicle.setName("BMW");
         return vehicle;
     }
+
+    @Bean
+    public Vehicle vehicle2(){
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("AUDI");
+        return vehicle;
+    }
+    @Bean
+    public Vehicle vehicle3(){
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("VIN");
+        return vehicle;
+    }
+
 
     @Bean
     public String hello(){
--- src/main/java/main/ExampleBean.java
+++ src/main/java/main/ExampleBean.java
@@ -6,17 +6,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class ExampleBean {
     public static void main(String[] args) {
-        Vehicle vehicle = new Vehicle();
-        vehicle.setName("Honda");
-        System.out.println("Vehicle name from non-spring context is: "+vehicle.getName());
-
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Vehicle vehicleBean = context.getBean(Vehicle.class);
+//        Vehicle vehicleBean = context.getBean(Vehicle.class); => return exception NoUniqueBeanDefinitionException
+        Vehicle vehicleBean = context.getBean("vehicle1",Vehicle.class);
         System.out.println("Vehicle name from spring Context is: "+vehicleBean.getName());
-
-        String hello = context.getBean(String.class);
-        System.out.println("String value from Spring Context is: "+ hello);
-        Integer num = context.getBean(Integer.class);
-        System.out.println("Integer value from Spring Context is: "+num);
     }
 }
